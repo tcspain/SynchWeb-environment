2f0d9f6e85c1d8bf2d23259f8d97e88776c3b499
Timothy Spain <timothy.spain@diamond.ac.uk>
2016-09-09 16:26:24 +0100

    A sample page using Marionette. Uses rather different start-up code to SynchWeb, and the templates are defined in index.html.

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,33 @@
-define(["jquery"], function($) {
-    $(function() {
-	$("body");
+app.module("Main", function (Main) {
+    Main.views = {};
+
+    app.addInitializer(function() {
+	Main.show();
     });
+
+    this.show = function() {
+	this.headerView = new this.views.HeaderView();
+	this.footerView = new this.views.FooterView();
+	this.bodyView = new this.views.BodyView();
+
+	app.header.show(this.headerView);
+	app.body.show(this.bodyView);
+	app.footer.show(this.footerView);
+    };
+
+    // views
+
+    Main.views.BodyView = Marionette.ItemView.extend({
+	template: "#body"
+    });
+
+    Main.views.FooterView = Marionette.ItemView.extend({
+	template: "#footer"
+    });
+
+    Main.views.HeaderView = Marionette.ItemView.extend({
+	template: "#header"
+    });
+
 });
+    
--- client/js/main.js
+++ client/js/main.js
@@ -1,11 +1,40 @@
 require.config({
     "baseUrl": "client/js",
     "paths": {
-	"app": "../main"
+	underscore: "vendor/underscore.min",
+	backbone: "vendor/backbone/backbone",
+	marionette: "vendor/backbone/backbone.marionette",
+	jquery: "vendor/jquery/jquery-1.9.1.min",
     },
     "shim": {
-	"jquery": ["jquery"]
+	"underscore": {
+	    exports: '_'
+	},
+	"backbone": {
+	    deps: ["underscore", "jquery"],
+	    exports: "Backbone"
+	},
+	"marionette": {
+	    deps: ["backbone"],
+	    exports: "Marionette"
+	},
     }
 });
 
-require(["app/main"]);
+require(["marionette"], function(Marionette) {
+    window.app = new Marionette.Application();
+
+    app.addRegions({
+	header: "#header-region",
+	footer: "#footer-region",
+	body: "#body-region"
+    });
+
+    require(["app"], function () {
+	app.start();
+    });
+
+});
+
+    
+    
